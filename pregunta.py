4f70e5fa9105920310054f7d4aa7426a119ab792
Yuliana Osorio <yosorio@unal.edu.co>
2022-11-23 21:18:44 -0600

    Laboratorio

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,28 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+import re
 import pandas as pd
-
+from datetime import datetime
 
 def clean_data():
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
+    
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
+        df[columna] = df[columna].str.lower()
+        df[columna] = df[columna].apply(lambda x: x.replace('_', ' '))
+        df[columna] = df[columna].apply(lambda x: x.replace('-', ' '))
 
-    #
-    # Inserte su código aquí
-    #
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(",", "")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "")
+    df['monto_del_credito'] = df['monto_del_credito'].astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+
+    df.drop_duplicates(inplace = True)
 
     return df
